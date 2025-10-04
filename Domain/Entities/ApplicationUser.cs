aeabda95cde25c452bd4c572b43b7f6baefd5449
Eddyberto <2020-0025@uteco.edu.do>
2025-10-03 20:14:38 -0400

    Se trabajo con el Domain; se crearon los DTOs, Entities, y Enums.

---
+++ Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MisFinanzas.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        // Navigation Properties
+        public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
+        public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+        public virtual ICollection<FinancialGoal> FinancialGoals { get; set; } = new List<FinancialGoal>();
+    }
+}
